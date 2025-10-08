a5bd8932ca3ec3586c3cd9355047292d5ef9a156
negociosfuturob <negociosfuturob@gmail.com>
2025-10-08 07:58:44 -0300

    Update SarandiMaisVerde.tsx

--- components/SarandiMaisVerde.tsx
+++ components/SarandiMaisVerde.tsx
@@ -1,1 +1,7 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 
+export default function SarandiMaisVerde() {
+  return (<div>Seu componente SarandiMaisVerde aqui</div>);
+}
