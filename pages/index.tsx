8c91cb9c476eb12e176c0419850edbe86bcfd368
negociosfuturob <negociosfuturob@gmail.com>
2025-10-08 07:53:24 -0300

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+// Arquivo: pages/index.tsx (Conteúdo Colado)
+
+import SarandiMaisVerde from './SarandiMaisVerde';
+// export default SarandiMaisVerde; <- Remova este 'export' duplicado
+// Arquivo: pages/index.js           <- Remova esta linha de comentário
+
+import React from 'react';
+
+function HomePage() {
+  return <h1>Olá Vercel! O Build vai funcionar agora.</h1>;
+}
+
+export default HomePage;
